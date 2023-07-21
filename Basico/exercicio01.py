2f3b2aa3809b419b1e366e120abbcc37ccf20c73
Arthur André <arthurdasilvaandre@gmail.com>
2023-07-21 19:31:53 -0300

    Desafio sobre variáveis

---
+++ Basico/exercicio01.py
@@ -0,0 +1,13 @@
+nome='Loucura'
+sobrenome='Rio21'
+idade=21
+ano_nascimento=2002
+maior_de_idade = idade>=18
+altura_metros=1.58
+
+print('Nome:',nome)
+print('Sobrenome',sobrenome)
+print('Idade',idade)
+print('Ano de nascimento:',ano_nascimento)
+print('É maior de idade?',maior_de_idade)
+print('Altura em metros:',altura_metros)
