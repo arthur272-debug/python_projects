80a4b399e91ed5a9fccb7486d6283df0bebdb103
Arthur André <arthurdasilvaandre@gmail.com>
2024-01-18 14:44:52 -0300

    add aulas

---
+++ aulas/aula__xxx.py
@@ -0,0 +1,1 @@
+# Revisão sobre o operador OR
---
+++ aulas/aula_xx.py
@@ -0,0 +1,16 @@
+#revisão sobre o operador AND
+print('Olá Mundo!!')
+
+#entrada = input('[E]ntrar [S]air: ')
+#senha_digitada = input('Senha: ')
+
+senha_permitida ='123456789'
+
+#if entrada == 'E' and senha_digitada == senha_permitida:
+#    print('Entrar')
+#else:
+#   print('Sair') 
+
+print(True and True)
+print(True and 1)
+print(False and False)
