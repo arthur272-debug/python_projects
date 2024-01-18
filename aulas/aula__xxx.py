8a8ad015f67edeb1e6d45a0d5881b3e4e2978b37
Arthur André <arthurdasilvaandre@gmail.com>
2024-01-18 15:08:37 -0300

    Update aula__xxx.py

--- aulas/aula__xxx.py
+++ aulas/aula__xxx.py
@@ -1,1 +1,17 @@
 # Revisão sobre o operador OR
+
+# entrada = input('[E]ntrar [S]air: ')
+# senha_digitada = input('Senha: ')
+
+# senha_permitida ='123456789'
+
+# if (entrada == 'E' or entrada =='e') and (senha_digitada == senha_permitida):
+#     print('Entrar')
+# else:
+#    print('Sair') 
+   
+# Avaliação de curto circuito
+# print(0 or False or 0 or 'abc')
+
+senha = input('Senha: ') or 'Sem senha'
+print(senha)
