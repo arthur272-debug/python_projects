1d3afb2bb58d849c453a76193a69cbeb4331623a
Arthur André <arthurdasilvaandre@gmail.com>
2024-01-22 12:26:03 -0300

    Update aula19.py

--- Aulas/aula19.py
+++ Aulas/aula19.py
@@ -8,8 +8,8 @@
 # if (entrada == 'E' or entrada =='e') and (senha_digitada == senha_permitida):
 #     print('Entrar')
 # else:
-#    print('Sair') 
-   
+#    print('Sair')
+
 # Avaliação de curto circuito
 # print(0 or False or 0 or 'abc')
 
