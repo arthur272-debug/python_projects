ea33cabf12464e186cc97c2b0b8f80ce565d1640
Arthur André <arthurdasilvaandre@gmail.com>
2024-01-24 11:27:20 -0300

    Update aula21.py

--- Aulas/aula21.py
+++ Aulas/aula21.py
@@ -1,2 +1,17 @@
 # Operadores in e not in
 
+# nome = 'Tutu'
+# print(nome[2])
+# print(nome[-3])
+# print('a' not in nome)
+# print('a' in nome)
+# print('u' in nome)
+# print('tu' in nome)
+
+nome = input("Digite um nome: ")
+encontrar = input("Digite o que encontrar: ")
+
+if (encontrar in nome):
+    print(f'{encontrar} está em {nome}.')
+else:
+    print(f"{encontrar} não está em {nome}.")
