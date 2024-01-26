72e06bf490c8ce9f838a01962440ccf1fbb5b557
Arthur André <arthurdasilvaandre@gmail.com>
2024-01-26 13:27:07 -0300

    Update aula24.py

--- Aulas/aula24.py
+++ Aulas/aula24.py
@@ -1,2 +1,17 @@
-# Fatiamento de strings - continuar em 25/01
+#Fatiamento de strings 
+'''
+Fatiamento [i:f:p] [::]
 
+len -> conta os caracteres de uma variável
+
+''' 
+variavel = 'Olá mundo'
+print(variavel[5])
+print(variavel[-3])
+print(variavel[4:8])
+print(variavel[-8:-2])
+print(variavel[-8:-2:2])
+print(variavel[::-1])
+
+len = len(variavel[-8:-2])
+print(len)
