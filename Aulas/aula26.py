b49c9574f1a55afe78fd6267f653879b09d7ec7e
Arthur André <arthurdasilvaandre@gmail.com>
2024-01-29 14:58:37 -0300

    aula_variaveis_constantes

---
+++ Aulas/aula26.py
@@ -0,0 +1,10 @@
+# Revisão sobre constantes e variáveis
+
+#Muitas condições no mesmo if (ruim)
+
+velocidade = 61  # velocidade atual do carro
+local_carro = 101  # local em que o carro está na estrada
+
+RADAR_1= 60  # velocidade máxima do radar 1
+LOCAL_1= 100  # local onde o radar 1 está
+RADAR_RANGER= 1  # A distância onde o radar pega
