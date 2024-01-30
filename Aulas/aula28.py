c196b5d99a61a328792b50044a5ed5da1d128c09
Arthur André <arthurdasilvaandre@gmail.com>
2024-01-30 14:23:09 -0300

    aula_sobre_flag_none_isNot_is

---
+++ Aulas/aula28.py
@@ -0,0 +1,19 @@
+# Aula sobre a Flag, None, is e is not 
+
+condicao = False
+passou_no_if= None
+
+if condicao:
+    passou_no_if = True
+    print('Faça algo')
+else: 
+    print('Não faça algo')
+
+# print(passou_no_if, passou_no_if is None)
+# print(passou_no_if, passou_no_if is not None)
+
+if passou_no_if is None:
+    print('Não passou no if')
+
+if passou_no_if is not None:
+    print('Passou no if')
