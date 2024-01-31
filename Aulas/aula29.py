0f7f59bc2af8284f1c0ea1275ee9be73f5c75b21
Arthur André <arthurdasilvaandre@gmail.com>
2024-01-31 14:37:48 -0300

    Create aula29.py

---
+++ Aulas/aula29.py
@@ -0,0 +1,10 @@
+# conversa sobre Tipos imutáveis; built-in; e métodos de strings.
+"""
+https://docs.python.org/pt-br/3/library/stdtypes.html  - documentação geral do Python
+Imutáveis que vimos: str, int, float, bool
+"""
+string = '1000'
+# outra_variavel = f'{string[:3]}ABC{string[4:]}'
+# print(string)
+# print(outra_variavel)
+print(string.zfill(10))
