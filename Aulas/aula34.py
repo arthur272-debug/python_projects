b689fde0668dc7474922071b74dc1097f98a88ee
Arthur André <arthurdasilvaandre@gmail.com>
2024-02-07 12:51:16 -0300

    Update aula34.py

--- Aulas/aula34.py
+++ Aulas/aula34.py
@@ -1,2 +1,14 @@
-# while dentro de outro while --- terminar no dia 06/02
+# while dentro de outro while 
 
+qtd_linhas = 5
+qtd_colunas = 5
+
+linha = 1
+while linha <= qtd_linhas:
+    coluna = 1
+    while coluna <= qtd_colunas:
+        print(f'{linha=} {coluna=}')
+        coluna +=1
+    linha +=1
+
+print('Acabou')
