12b4ed82e3c298af0d1a3dedd244dc3c3684f3a4
Arthur André <arthurdasilvaandre@gmail.com>
2024-02-08 12:31:11 -0300

    Update exercicio08.py

--- Basico/exercicio08.py
+++ Basico/exercicio08.py
@@ -1,1 +1,21 @@
-# Exercício guiado 
+# Exercício guiado - Iterando strings com o while
+
+nome ='Tutuão Fabroso'
+
+tamanho_nome = len(nome)
+print('Tamanho do nome:',tamanho_nome)
+contador =0
+
+while contador<tamanho_nome:
+    print(nome[contador])
+    contador +=1
+
+nova_string=''
+contador=0
+
+while contador<tamanho_nome:
+    letra = nome[contador]
+    nova_string+=letra
+    contador+=1
+
+print(nova_string)
