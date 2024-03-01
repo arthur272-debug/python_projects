0db04c19e3bdc00f058c46bd95f2ade62b01aeac
Arthur André <arthurdasilvaandre@gmail.com>
2024-03-01 14:55:15 -0300

    desempacotar__empacotar

---
+++ Aulas_Basico/aula46.py
@@ -0,0 +1,10 @@
+# Aula sobre desempacotamento e empacotamento -- terminar isso
+
+nome1,nome2,nome3 = ['Maria','João','Luiz']
+print(nome2)
+
+titulo, *resto = ['Sr.','YouTube',3,5]
+print(titulo,resto)
+
+dados = (1,2,4,5,6,'ggffg')
+print(dados)
