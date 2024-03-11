f0b0981fb4fafdac03ebd38f13f0a673304c60d0
Arthur André <arthurdasilvaandre@gmail.com>
2024-03-11 14:44:16 -0300

    Update aula49.py

--- Aulas_Basico/aula49.py
+++ Aulas_Basico/aula49.py
@@ -1,5 +1,15 @@
 # Imprecisão do ponto flutuante
 
-numero_01 = 0.1
+import decimal
+
+
+numero_01 = 0.5
 numero_02 = 0.7
 numero_03 = numero_01+numero_02
+numero_04 = decimal.Decimal('0.2')
+numero_05 = decimal.Decimal('0.5')
+numero_06 = numero_04+ numero_05
+print(numero_03)
+print(numero_06)
+print(f'{numero_03:.2f}')
+print(round(numero_03,2))
