645452bad1045125214cecab0956a510a6c97962
Arthur André <arthurdasilvaandre@gmail.com>
2024-03-12 14:52:23 -0300

    Create aula51.py

---
+++ Aulas_Basico/aula51.py
@@ -0,0 +1,14 @@
+# aula sobre lista de listas e seus índices
+
+salas = [
+
+    ['Maria', 'Helena'],
+    ['Elaine'],
+    ['Luiz', 'João', 'Eduarda', (0, 10, 20, 30, 40)]
+
+]
+
+print(salas[1][0])
+print(salas[0][1])
+print(salas[2][2])
+print(salas[2][3][2])
