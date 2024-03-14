b90b131d142d69d4db718527ac2f829ae5866250
Arthur André <arthurdasilvaandre@gmail.com>
2024-03-14 14:52:12 -0300

    Update aula52.py

---
+++ Aulas_Basico/aula52.py
@@ -0,0 +1,8 @@
+# comandos do interpretador no terminal
+'''
+python mod.py (executa o mod)
+python -u (unbuffered)
+python -m mod (lib mod como script)
+python -c 'cmd' (comando)
+python -i mod.py (interativo com mod)
+'''
