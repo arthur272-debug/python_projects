f871aaeff7a4537b85c45c48b4e4b00798660152
Arthur André <arthurdasilvaandre@gmail.com>
2024-04-05 11:30:35 -0300

    Update aula55.py

--- Aulas_Intermediario/aula55.py
+++ Aulas_Intermediario/aula55.py
@@ -1,2 +1,24 @@
 # Revisão sobre funções
 
+def Print(a, b, c):
+    print('Exemplo 1')
+    print('Exemplo 2')
+    print('Exemplo 3')
+    print('Exemplo 4')
+
+
+def imprimir(a, b, c):
+    print(a, b, c)
+
+
+imprimir(1, 2, 3)
+imprimir(4, 5, 6)
+
+
+def saudacao(nome='Seu nome'):
+    print(f'Olá {nome}!')
+
+
+saudacao('Arthur')
+saudacao('Tutu')
+saudacao('Felipão')
