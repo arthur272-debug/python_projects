d15ac7c6981a3662ffb1dd1373e81c6e39f48093
Arthur André <arthurdasilvaandre@gmail.com>
2024-04-10 20:40:44 -0300

    code completo

--- Aulas_Intermediario/aula57.py
+++ Aulas_Intermediario/aula57.py
@@ -1,2 +1,13 @@
-#Valores padrão para funções Python + NoneType e None
+# Valores padrão para funções Python + NoneType e None
 
+def soma(x, y, z=None):
+    if z is not None:
+        print(f'{x=} {y=} {z=}', x+y+z)
+    else:
+        print(f'{x=} {y=}', x+y)
+
+
+soma(2, 5, 8)
+soma(26, 50)
+soma(80, 50)
+soma(90, 80, 100)
