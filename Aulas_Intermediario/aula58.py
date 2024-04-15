853e896f1dbdbb3a40912c00e93286a084bb03e1
Arthur André <arthurdasilvaandre@gmail.com>
2024-04-15 12:56:24 -0300

    Update aula58.py

--- Aulas_Intermediario/aula58.py
+++ Aulas_Intermediario/aula58.py
@@ -1,1 +1,22 @@
 # Escopo de funções em Python
+
+x = 1
+
+
+def escopo():
+    global x
+    x = 10
+
+    def outra_funcao():
+        global x
+        x = 11
+        y = 2
+        print(x, y)
+
+    outra_funcao()
+    print(x)
+
+
+print(x)
+escopo()
+print(x,0)
