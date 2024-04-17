67591d88a25350e597c857c024df6833d27193ab
Arthur André <arthurdasilvaandre@gmail.com>
2024-04-17 13:14:48 -0300

    Update aula59.py

--- Aulas_Intermediario/aula59.py
+++ Aulas_Intermediario/aula59.py
@@ -1,1 +1,14 @@
 # Return de valores de funções
+
+def soma(x, y):
+    if x > 10:
+        return [10, 20]
+    return x+y
+
+
+soma1 = soma(10, 30)
+soma2 = soma(1, 2)
+
+print(soma1)
+print(soma2)
+print(soma(55, 77))
