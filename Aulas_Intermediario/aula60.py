21afd09f5ac42d865fe1707bae785dd0a6a83237
Arthur André <arthurdasilvaandre@gmail.com>
2024-04-19 14:15:48 -0300

    Update aula60.py

--- Aulas_Intermediario/aula60.py
+++ Aulas_Intermediario/aula60.py
@@ -1,1 +1,18 @@
-# aula sobre args
+# aula sobre args - argumentos não nomeados
+
+def soma(*args):
+    total = 0
+    for numero in args:
+        total += numero
+    return total
+
+
+soma1 = soma(1, 2, 3, 4)
+print(soma1)
+
+soma3 = soma(4, 5, 6)
+print(soma3)
+
+numeros = 1, 2, 3, 4, 5, 6
+outra_soma = soma(*numeros)
+print(outra_soma)
