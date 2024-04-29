2a02f0b586cf24b6d5046d4ef79ba2ccaee7767b
Arthur André <arthurdasilvaandre@gmail.com>
2024-04-29 14:41:57 -0300

    Update aula61.py

--- Aulas_Intermediario/aula61.py
+++ Aulas_Intermediario/aula61.py
@@ -1,1 +1,17 @@
 # Higher Order Functions  - Conceitos
+
+def saudacao(msg, nome):
+    return f'{msg}, {nome}'
+
+
+def executa(funcao, *args):
+    return funcao(*args)
+
+
+print(
+    executa(saudacao, 'Bom dia', 'Luiz')
+)
+
+print(
+    executa(saudacao, 'Bom dia', 'Maria')
+)
