6c1be277e98f4302e0561537fadaf628a2b8fff9
Arthur André <arthurdasilvaandre@gmail.com>
2024-05-15 15:42:57 -0300

    review_dict

---
+++ Aulas_Intermediario/aula63.py
@@ -0,0 +1,13 @@
+# Revisão de dicionários
+
+pessoa = {
+
+    'nome': 'TutuBala',
+    'Idade': 22,
+    'Estado': 'Rio de Janeiro',
+
+}
+
+print(type(pessoa))
+print(pessoa['nome'])
+print(pessoa['Estado'])
