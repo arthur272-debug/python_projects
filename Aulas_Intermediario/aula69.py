6e9485cb05d1afa09e24033c7e7cd5a05e9351e9
Arthur André <arthurdasilvaandre@gmail.com>
2024-05-21 20:19:06 -0300

    manipulando_conjunto

---
+++ Aulas_Intermediario/aula69.py
@@ -0,0 +1,9 @@
+# Revisando métodos úteis
+
+lista1 = set()
+lista1.add('Bala01')
+lista1.add(1)
+lista1.update(('Hello World', 1, 3, 5, 6))
+lista1.discard('Hello World')
+lista1.discard('Bala01')
+print(lista1)
