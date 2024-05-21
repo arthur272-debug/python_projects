06a1c4914c5e4553f6f1042c19436d1a0fabe641
Arthur André <arthurdasilvaandre@gmail.com>
2024-05-21 19:58:57 -0300

    pontos_interessantes_set

---
+++ Aulas_Intermediario/aula68.py
@@ -0,0 +1,12 @@
+# Pontos interessantes sobre o tipo Set
+
+lista1 = [1, 2, 3, 3, 3, 3, 3, 5]
+conjunto1 = set(lista1)
+lista2 = list(conjunto1)
+conjunto2 = {1, 2, 3}
+
+print(conjunto1)
+print(lista2)
+print(conjunto2)
+
+print(3 not in conjunto1)
