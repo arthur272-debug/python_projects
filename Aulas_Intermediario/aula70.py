1ca0bf3607cfa073e524e76e13cfc5fa264fbc30
Arthur André <arthurdasilvaandre@gmail.com>
2024-06-03 19:40:34 -0300

    operando_conjuntos

---
+++ Aulas_Intermediario/aula70.py
@@ -0,0 +1,15 @@
+# Operações  sobre conjuntos
+
+conjunto1 = {1, 2, 3}
+conjunto2 = {2, 3, 4}
+print(conjunto1)
+print(conjunto2)
+
+conjunto3 = conjunto1 | conjunto2
+print('1:', conjunto3)
+conjunto3 = conjunto1 & conjunto2
+print('2:', conjunto3)
+conjunto3 = conjunto2 - conjunto1
+print('3:', conjunto3)
+conjunto3 = conjunto1 ^ conjunto2
+print('4:', conjunto3)
