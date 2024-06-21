0c76c877a050f5da759f8163ddf738065dbb5c9a
Arthur André <arthurdasilvaandre@gmail.com>
2024-06-21 10:05:38 -0300

    revisando_iterable_e_iterator

---
+++ Aulas_Intermediario/aula83.py
@@ -0,0 +1,11 @@
+# Aula sobre Generator expression, Iterables e Iterators em Python
+
+iterable = ['Yo', 'Tenho', '__iter__']
+iterator = iter(iterable)
+
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
