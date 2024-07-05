07325adaa0af9f967f3a2939770e469aac33f141
Arthur André <arthurdasilvaandre@gmail.com>
2024-07-05 11:57:08 -0300

    diferenca_generator_expression_e_lista

--- Aulas_Intermediario/aula84.py
+++ Aulas_Intermediario/aula84.py
@@ -1,1 +1,12 @@
-# Aula sobre Generator expression, Iterables e Iterators em Python - parte 2 - terminar
+# Aula sobre Generator expression, Iterables e Iterators em Python - Parte 2
+
+import sys
+
+lista = [n for n in range(10000)]
+generator2 = (n for n in range(10000))
+
+print(lista)
+print(sys.getsizeof(lista))  # tamanho armazenado na memória
+
+print(generator2)
+print(sys.getsizeof(generator2))  # tamanho armazenado na memória
