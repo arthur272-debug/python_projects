d870455b00246de95655161dbde1f6748f263f2c
Arthur André <arthurdasilvaandre@gmail.com>
2024-09-17 19:41:06 -0300

    aula_importlib

--- Aulas_Intermediario/aula93.py
+++ Aulas_Intermediario/aula93.py
@@ -1,1 +1,13 @@
 # aula sobre recarregando módulos, importlib e singleton
+import importlib
+
+import aula93_m
+
+print(aula93_m.metro)
+
+for i in range(10):
+    importlib.reload(aula93_m)
+    print(i)
+
+
+print('Terminou')
---
+++ Aulas_Intermediario/aula93_m.py
@@ -0,0 +1,4 @@
+print('exemplo1')
+print('Tuturito')
+
+metro = 1
