773767a36aa2ddb5551976dd8f6b17c90448a6f4
Arthur André <arthurdasilvaandre@gmail.com>
2024-09-28 17:32:07 -0300

    modularizando_pacotes

--- Exercicios_Intermediario/exercicio18.py
+++ Exercicios_Intermediario/exercicio18.py
@@ -9,32 +9,24 @@
 # Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
 
 # Resolução dos exercícios
+from dados import produtos  # tem que ter o __init__.py - para ter nenhum problema
 import copy
 
-produtos = [
-    {'nome': 'Produto 5', 'preco': 10.00},
-    {'nome': 'Produto 1', 'preco': 22.32},
-    {'nome': 'Produto 3', 'preco': 10.11},
-    {'nome': 'Produto 2', 'preco': 105.87},
-    {'nome': 'Produto 4', 'preco': 69.90},
-]
-
+print('Lista de produtos originais:\n', produtos, '\n')
 # 1)
 novos_produtos = copy.deepcopy(produtos)
 for novo_preco in novos_produtos:
-    novo_preco['preco'] = novo_preco['preco'] * 0.10  # 10%
+    novo_preco['preco'] = round(novo_preco['preco'] * 1.1, 2)  # 10% -> 1.1
 print('Lista dos novos produtos com o aumento de 10%:\n', novos_produtos, '\n')
 
 # 2)
-produtos_ordenados_por_nome = copy.deepcopy(produtos)
 produtos_ordenados_por_nome = sorted(
-    produtos_ordenados_por_nome, key=lambda x: x['nome'], reverse=True)
+    copy.deepcopy(produtos), key=lambda x: x['nome'], reverse=True)
 print('Lista dos produtos ordenados pelo nome em ordem descrescente:\n',
       produtos_ordenados_por_nome, '\n')
 
 # 3)
-produtos_ordenados_por_preco = copy.deepcopy(produtos)
 produtos_ordenados_por_preco = sorted(
-    produtos_ordenados_por_preco, key=lambda x: x['preco'])
+    copy.deepcopy(produtos), key=lambda x: x['preco'])
 print('Lista dos produtos ordenados pelo preço em ordem crescente:\n',
       produtos_ordenados_por_preco, '\n')
