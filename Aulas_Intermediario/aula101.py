ea13b5b7ccdfe39a9a346b1ebf815a1b1df8d3de
Arthur André <arthurdasilvaandre@gmail.com>
2024-12-27 17:31:58 -0300

    entendendo_zip_e_zipLongest

--- Aulas_Intermediario/aula101.py
+++ Aulas_Intermediario/aula101.py
@@ -1,1 +1,9 @@
-# Entendendo sobre Zip e zip-longest -ver e praticar um exemplo no chatgpt
+# Entendendo sobre Zip e zip-longest 
+from itertools import zip_longest
+
+lista1 = [1, 2, 3, 4, 5, 6, 7]
+lista2 = ['oi', 'tudo', 'bem', 'com', 'você']
+combinado = zip(lista1, lista2)
+combinado_inverso= zip_longest(lista1, lista2)
+print(f'Ordem normal: {list(combinado)}')
+print(f'Ordem inversa {list(combinado_inverso)}')
