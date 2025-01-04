a9180117cc61a570837759c4e2b25f10c458e146
Arthur André <arthurdasilvaandre@gmail.com>
2025-01-04 16:39:23 -0300

    praticando_groupby

--- Aulas_Intermediario/aula104.py
+++ Aulas_Intermediario/aula104.py
@@ -1,1 +1,29 @@
 # groupby - itertools
+
+from itertools import groupby
+
+alunos =[
+    {'nome': 'Luiz', 'nota': 'A'},
+    {'nome': 'Letícia', 'nota': 'B'},
+    {'nome': 'Fabrício', 'nota': 'A'},
+    {'nome': 'Rose', 'nota': 'C'},
+    {'nome': 'Joana', 'nota': 'B'},
+    {'nome': 'Carlos', 'nota': 'A'},
+    {'nome': 'Julia', 'nota': 'A'},
+    {'nome': 'Zeca', 'nota': 'C'},
+    {'nome': 'Marcos', 'nota': 'B'},
+    {'nome': 'Eduardo', 'nota': 'A'},
+    {'nome': 'Luiza', 'nota': 'D'},
+]
+
+def ordenarAluno(aluno):
+    return aluno['nota']
+
+alunos_agrupados = sorted(alunos, key=ordenarAluno)
+grupos_notas = groupby(alunos_agrupados, key=ordenarAluno)
+
+print('Notas dos alunos:')
+for nota, grupo in grupos_notas:
+    print(f'\nNota {nota}:')
+    for aluno in grupo:
+        print(f'\t{aluno["nome"]}')
