ebcb67dd806215343a0c703d54b1e1efb66d1db4
Arthur André <arthurdasilvaandre@gmail.com>
2025-01-11 20:23:13 -0300

    praticando_filter

--- Aulas_Intermediario/aula106.py
+++ Aulas_Intermediario/aula106.py
@@ -1,1 +1,16 @@
 # aprendendo sobre filter que é um filtro funcional
+
+produtos =[
+    {'nome': 'p1', 'preco': 13.20},
+    {'nome': 'p2', 'preco': 55.20},
+    {'nome': 'p3', 'preco': 5.20},
+    {'nome': 'p4', 'preco': 22.20},
+    {'nome': 'p5', 'preco': 100.67},   
+]
+
+def filtrarProduto(produto):
+    return produto['preco'] > 20
+
+produtosFiltrados = filter(filtrarProduto, produtos)
+print(produtosFiltrados)
+print(list(produtosFiltrados))
