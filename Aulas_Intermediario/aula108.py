25dfd40b5bc381dd267eabbb997d9632aa58e650
Arthur André <arthurdasilvaandre@gmail.com>
2025-01-18 18:40:41 -0300

    praticando_recursao

--- Aulas_Intermediario/aula108.py
+++ Aulas_Intermediario/aula108.py
@@ -1,1 +1,12 @@
 # Relembrando funções recursivas e recursividade
+
+def fazerRecursao(inicio=0,fim=10):
+    if inicio >= fim:
+        return fim
+    
+    print(inicio,fim)
+    
+    inicio += 1
+    return fazerRecursao(inicio,fim)
+
+print(fazerRecursao())
