96e012242b52c13ce701fdc0a147bdd6eb81747c
Arthur André <arthurdasilvaandre@gmail.com>
2025-02-01 14:18:57 -0300

    praticando_mais_recursao

--- Aulas_Intermediario/aula109.py
+++ Aulas_Intermediario/aula109.py
@@ -1,1 +1,15 @@
 # Praticando mais sobre funções recursivas
+
+import sys
+
+sys.setrecursionlimit(3000)
+
+def fazerFatorial(numero):
+    if numero <= 1:
+        return 1
+    else:
+        return numero * fazerFatorial(numero - 1)
+    
+print(fazerFatorial(5))
+print(fazerFatorial(10))
+print(fazerFatorial(1000))
