cbb49f152f6ff0fb886d0a0a638640dc550e2cac
Arthur André <arthurdasilvaandre@gmail.com>
2025-02-04 21:27:51 -0300

    praticando_criando_ambiente_virutal

--- Aulas_Intermediario/aula110/aula110.py
+++ Aulas_Intermediario/aula110/aula110.py
@@ -1,1 +1,6 @@
 # aprendendo sobre Ambientes virtuais
+
+# O instrutor ensina a criar um ambiente virtual e explica a importância 
+# de se criar um ambiente virtual para cada projeto
+
+# Foi criado um ambiente virtual chamado 'venv' nessa pasta
