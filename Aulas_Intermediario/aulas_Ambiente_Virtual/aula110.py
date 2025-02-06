bfcce49b165db757bd9d0ab39dde4066e374d31f
Arthur André <arthurdasilvaandre@gmail.com>
2025-02-06 20:33:46 -0300

    adicionando_novas_informacoes

--- Aulas_Intermediario/aulas_Ambiente_Virtual/aula110.py
+++ Aulas_Intermediario/aulas_Ambiente_Virtual/aula110.py
@@ -4,3 +4,6 @@
 # de se criar um ambiente virtual para cada projeto
 
 # Foi criado um ambiente virtual chamado 'venv' nessa pasta
+
+# Também foi aprendido sobre o pip e como instalar pacotes com ele
+# Foi feita a instalação de alguns pacotes como pymysql, por exemplo
