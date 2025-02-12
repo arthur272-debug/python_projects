4a1dbc835ea74c8ef36bd4521b6e3152acf0f5d1
Arthur André <arthurdasilvaandre@gmail.com>
2025-02-11 21:28:21 -0300

    praticando_context_manager_with

--- Aulas_Intermediario/aula_Criando_Arquivo/aula112.py
+++ Aulas_Intermediario/aula_Criando_Arquivo/aula112.py
@@ -2,3 +2,13 @@
 
 caminho_arquivo ='D:\\Docs_Arthur\\Projetos_cursos\\python_projects\\Aulas_Intermediario\\aula_Criando_Arquivo\\'
 caminho_arquivo += 'aula112_arquivo.txt'
+
+# Uma das formas de criar um arquivo
+# arquivo = open(caminho_arquivo, 'w')
+# arquivo.close()
+
+# Outra forma de criar um arquivo - padrão 
+with open(caminho_arquivo, 'w') as arquivo:
+    print('Arquivo criado com sucesso!')
+    print('Hello, World!')
+    print('Arquivo vai ser fechado')
