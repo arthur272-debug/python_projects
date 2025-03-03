d5a77360a90662743c80483e847ddde2e0e5c5d2
Arthur André <arthurdasilvaandre@gmail.com>
2025-03-03 15:14:30 -0300

    praticando_aula

--- Aulas_Intermediario/aula117.py
+++ Aulas_Intermediario/aula117.py
@@ -1,1 +1,18 @@
 # Aprendendo sobre o problema dos parâmetros mutáveis em funções
+
+def adicionarCliente(nome, lista=None):
+    if lista is None:
+        lista = []
+    lista.append(nome)
+    return lista
+
+cliente1 = adicionarCliente('Arthur')
+adicionarCliente('Josés',cliente1)
+adicionarCliente('José',cliente1)
+adicionarCliente('Tutu',cliente1)
+
+cliente2 = adicionarCliente('Maria')
+adicionarCliente('Joana',cliente2)
+adicionarCliente('João',cliente2)
+print(cliente1)
+print(cliente2)
