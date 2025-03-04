c173e0eea5a3dfcd62e85c829f2cab39d8067916
Arthur André <arthurdasilvaandre@gmail.com>
2025-03-04 14:48:37 -0300

    desenvolvendo_exercicio

--- Exercicios_Intermediario/exercicio22.py
+++ Exercicios_Intermediario/exercicio22.py
@@ -3,3 +3,43 @@
 # - O usuário deve poder escolher entre adicionar uma nova tarefa, listar as tarefas, 
 # desfazer a última tarefa (desfazer), refazer a última tarefa (refazer). 
 # O comando sair do programa é algo extra.
+
+comando = None
+tarefas = []
+tarefas_desfeitas = []
+
+while True:
+    print("Lista de tarefas - Escolha uma opção:\n")
+    print("1 - Adicionar tarefa;")
+    print("2 - Listar tarefas;")
+    print("3 - Desfazer;")
+    print("4 - Refazer; e")
+    print("5 - Sair do programa.\n")
+    
+    comando = input("Digite o número da opção desejada: ")
+    
+    if comando == "1":
+        tarefa = input("Digite a tarefa a ser adicionada: ")
+        tarefas.append(tarefa)
+    elif comando == "2":
+        print("Tarefas:")
+        for tarefa in tarefas:
+            print(f"- {tarefa}")
+    elif comando == "3":
+        if len(tarefas) == 0:
+            print("Não há tarefas para desfazer.")
+        else:
+            tarefa_desfeita = tarefas.pop()
+            for tarefa in tarefas:
+                print(f"- {tarefa}")
+    elif comando == "4":
+        if len(tarefas_desfeitas) == 0:
+            print("Não há tarefas para refazer.")
+        else:
+            tarefas = tarefas_desfeitas.pop()
+            for tarefa in tarefas:
+                print(f"- {tarefa}")
+    elif comando == "5":
+        break
+    else:
+        print("Comando inválido.")
