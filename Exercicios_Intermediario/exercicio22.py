2eadc8d58313e3174808f5fcce7db1b0158da281
Arthur André <arthurdasilvaandre@gmail.com>
2025-03-04 15:17:23 -0300

    fazendo_correcoes

--- Exercicios_Intermediario/exercicio22.py
+++ Exercicios_Intermediario/exercicio22.py
@@ -8,6 +8,14 @@ comando = None
 tarefas = []
 tarefas_desfeitas = []
 
+def listar_tarefas():
+    if len(tarefas) == 0:
+            print("Não há tarefas para listar.")
+    else:
+        print("Tarefas:")
+        for tarefa in tarefas:
+            print(f"- {tarefa}")
+
 while True:
     print("Lista de tarefas - Escolha uma opção:\n")
     print("1 - Adicionar tarefa;")
@@ -21,25 +29,26 @@ while True:
     if comando == "1":
         tarefa = input("Digite a tarefa a ser adicionada: ")
         tarefas.append(tarefa)
+        listar_tarefas()
+        
     elif comando == "2":
-        print("Tarefas:")
-        for tarefa in tarefas:
-            print(f"- {tarefa}")
+            listar_tarefas()
+            
     elif comando == "3":
-        if len(tarefas) == 0:
-            print("Não há tarefas para desfazer.")
-        else:
-            tarefa_desfeita = tarefas.pop()
-            for tarefa in tarefas:
-                print(f"- {tarefa}")
+            tarefas_desfeitas.append(tarefas.pop())
+            listar_tarefas()
+            
     elif comando == "4":
         if len(tarefas_desfeitas) == 0:
             print("Não há tarefas para refazer.")
         else:
-            tarefas = tarefas_desfeitas.pop()
-            for tarefa in tarefas:
-                print(f"- {tarefa}")
+            tarefas.append(tarefas_desfeitas.pop())
+            listar_tarefas()
+            
     elif comando == "5":
         break
+    
     else:
         print("Comando inválido.")
+        
+    print("\n")
