4348af34c447e6d73ff54b269f69a4a95f7715ad
Arthur André <arthurdasilvaandre@gmail.com>
2025-03-04 14:28:36 -0300

    formatando_arquivo

---
+++ Exercicios_Intermediario/exercicio22.py
@@ -0,0 +1,5 @@
+# Exercício - Lista de tarefas com desfazer e refazer
+# Crie um programa que gerencie uma lista de tarefas. O programa deve:
+# - O usuário deve poder escolher entre adicionar uma nova tarefa, listar as tarefas, 
+# desfazer a última tarefa (desfazer), refazer a última tarefa (refazer). 
+# O comando sair do programa é algo extra.
