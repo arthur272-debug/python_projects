c10e4e3ef00daa2eec4cadd308ad131981fe2dbe
Arthur André <arthurdasilvaandre@gmail.com>
2025-04-12 14:57:22 -0300

    pratincando_positional_only_e_keyword_only

--- Aulas_Intermediario/aula119.py
+++ Aulas_Intermediario/aula119.py
@@ -1,1 +1,24 @@
 # aprendendo sobre Positional_Only parameters e Keyword_Only parameters
+
+
+# Positional_Only parameters
+def somarNumeros(a, b, /):
+    return a + b
+
+
+def somarNumeros2(a, b, /, c, d):
+    return a + b + c + d
+
+
+def somarNumeros3(a, b, *, c, d):
+    return a + b + c + d
+
+
+def somarNumeros4(*, a, b):
+    return a + b
+
+
+print(somarNumeros(1, 2))
+print(somarNumeros2(1, 2, 3, d=4))
+print(somarNumeros3(1, 2, c=9, d=41))
+print(somarNumeros4(a=1, b=2))
