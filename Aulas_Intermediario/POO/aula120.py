6dfe5828c16f202c309bea7be484280bb06cc7fc
Arthur André <arthurdasilvaandre@gmail.com>
2025-04-21 11:24:34 -0300

    praticando_class

--- Aulas_Intermediario/POO/aula120.py
+++ Aulas_Intermediario/POO/aula120.py
@@ -1,1 +1,16 @@
 # Revisando sobre classes
+
+
+class Pessoa: ...
+
+
+p1 = Pessoa()
+p1.nome = "Tutu"
+p1.sobrenome = "Lima"
+
+p2 = Pessoa()
+p2.nome = "Gancho"
+p2.sobrenome = "Pereira"
+
+print(p1.nome, p1.sobrenome)
+print(p2.nome, p2.sobrenome)
