bf82a916674bb52e2d3999dfa9f7d01509b5ff2b
Arthur André <arthurdasilvaandre@gmail.com>
2025-04-21 12:02:54 -0300

    aprendendo_init

--- Aulas_Intermediario/POO/aula121.py
+++ Aulas_Intermediario/POO/aula121.py
@@ -1,1 +1,12 @@
 # Revisando sobre o método __init__
+
+class Carro:
+    def __init__(self, nome, marca):
+        self.nome = nome
+        self.marca = marca
+
+c1 = Carro('Fusca', 'Volkswagen')
+c2 = Carro('Civic', 'Honda')
+
+print(c1.nome, c1.marca)
+print(c2.nome, c2.marca)
