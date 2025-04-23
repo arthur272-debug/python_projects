f046618b80537e8af16c04bc5814ad014e8e8746
Arthur André <arthurdasilvaandre@gmail.com>
2025-04-23 11:57:20 -0300

    praticando_revisao_conceito

--- Aulas_Intermediario/POO/aula123.py
+++ Aulas_Intermediario/POO/aula123.py
@@ -1,1 +1,23 @@
 # Revisando a parte de escopo - classe e métodos da classe
+
+
+class Animal:
+    def __init__(self, nome, especie):
+        self.nome = nome
+        self.especie = especie
+
+    variavel = "nome"
+    print(variavel)
+
+    def comer_alimento(self, alimento):
+        self.alimento = alimento
+        print(f"{self.nome} comeu {self.alimento}")
+
+    def executar(self, *args, **kwargs):
+        return self.comer_alimento(*args, **kwargs)
+
+
+cachorro = Animal(nome="Rex", especie="Cachorro")
+print(cachorro.nome)
+print(cachorro.especie)
+print(cachorro.executar("ração"))
