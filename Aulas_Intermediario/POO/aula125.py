707668244f4c81a74f2f85315de7b7d9a0509a12
Arthur André <arthurdasilvaandre@gmail.com>
2025-05-02 09:47:59 -0300

    revisando_conceito_atributo

--- Aulas_Intermediario/POO/aula125.py
+++ Aulas_Intermediario/POO/aula125.py
@@ -1,1 +1,17 @@
 # revisando os atributos de classe
+class Pessoa:
+    ano_atual = 2025
+
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def get_ano_nascimento(self):
+        return self.ano_atual - self.idade
+
+
+pessoa1 = Pessoa("Lucas", 25)
+pessoa2 = Pessoa("Maria", 30)
+
+print(f"{pessoa1.nome} nasceu em {pessoa1.get_ano_nascimento()}")
+print(f"{pessoa2.nome} nasceu em {pessoa2.get_ano_nascimento()}")
