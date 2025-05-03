711cca00ec486e27396d525673d90cb4f3360bd6
Arthur André <arthurdasilvaandre@gmail.com>
2025-05-03 12:24:29 -0300

    resolvendo_exercicio

--- Exercicios_Intermediario/POO/exercicio24/exercicio24_a.py
+++ Exercicios_Intermediario/POO/exercicio24/exercicio24_a.py
@@ -4,3 +4,32 @@
 # Salvando a classe em um arquivo json
 
 import json
+
+CAMINHO_ARQUIVO = (
+    "D:\\Docs_Arthur\\Projetos_cursos\\python_projects\\Exercicios_Intermediario\\POO"
+)
+
+
+class Carro:
+    def __init__(self, modelo, ano, cor, marca):
+        self.modelo = modelo
+        self.ano = ano
+        self.cor = cor
+        self.marca = marca
+
+
+carro1 = Carro("Fusca", 1980, "azul", "Volkswagen")
+carro2 = Carro("Civic", 2020, "preto", "Honda")
+carro3 = Carro("Civic", 2020, "preto", "Honda")
+
+dados = [vars(carro1), vars(carro2), vars(carro3)]
+
+
+def salvar_dados():
+    with open(CAMINHO_ARQUIVO + "/carros.json", "w") as arquivo:
+        json.dump(dados, arquivo, indent=4)
+
+
+if __name__ == "__main__":
+    salvar_dados()
+    print("Dados salvos com sucesso!")
--- Exercicios_Intermediario/POO/exercicio24/exercicio24_b.py
+++ Exercicios_Intermediario/POO/exercicio24/exercicio24_b.py
@@ -1,1 +1,14 @@
 # Recuperando o arquivo com os dados da classe e criando as suas instâncias
+
+import json
+from exercicio24_a import CAMINHO_ARQUIVO, Carro
+
+with open(CAMINHO_ARQUIVO + "/carros.json", "r") as arquivo:
+    dados = json.load(arquivo)
+    carro1 = Carro(**dados[0])
+    carro2 = Carro(**dados[1])
+    carro3 = Carro(**dados[2])
+
+print(carro1.__dict__)
+print(carro2.__dict__)
+print(carro3.__dict__)
