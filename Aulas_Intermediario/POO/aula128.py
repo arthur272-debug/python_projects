a41732ec432ddf01b812b3d250859bccca2cc4bc
Arthur André <arthurdasilvaandre@gmail.com>
2025-05-17 14:13:01 -0300

    praticando_staticmethod

--- Aulas_Intermediario/POO/aula128.py
+++ Aulas_Intermediario/POO/aula128.py
@@ -1,1 +1,16 @@
-# aprendendo sobre o @staticmethod 
+# aprendendo sobre o @staticmethod - geralmente usado para criar métodos que não precisam de acesso a instância ou classe
+
+
+class Pessoa:
+    def __init__(self, nome):
+        self.nome = nome
+
+    @staticmethod
+    def criar_pessoa(nome):
+        return Pessoa(nome)
+
+
+pessoa1 = Pessoa.criar_pessoa("Lucas")
+pessoa2 = Pessoa.criar_pessoa("João")
+print(pessoa1.nome)
+print(pessoa2.nome)
