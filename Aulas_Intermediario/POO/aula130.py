ca068e495fb70695058f3c62d9141bb5885c302b
Arthur André <arthurdasilvaandre@gmail.com>
2025-07-05 14:53:12 -0300

    ajeitando_arquivo

--- Aulas_Intermediario/POO/aula130.py
+++ Aulas_Intermediario/POO/aula130.py
@@ -1,5 +1,14 @@
 # aprendendo sobre @property - tipo um getter no python
 
+
 class Produto:
-    def __init__(self):
-        pass
+    def __init__(self, cor):
+        self._cor = cor
+
+    @property
+    def cor(self):
+        return self._cor
+
+
+produto1 = Produto("azul")
+print(produto1.cor)  # Acessando o método como se fosse um atributo
