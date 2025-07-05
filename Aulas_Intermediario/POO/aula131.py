b6643693a186014df80464a5e1d4f3c4b5a02ec9
Arthur André <arthurdasilvaandre@gmail.com>
2025-07-05 15:23:21 -0300

    praticando_conceitos

--- Aulas_Intermediario/POO/aula131.py
+++ Aulas_Intermediario/POO/aula131.py
@@ -1,1 +1,30 @@
 # aprendendo sobre @property + @setter -> getter e setter
+
+
+class Caneta:
+    def __init__(self, cor, cor_tampa):
+        self._cor = cor
+        self._cor_tampa = cor_tampa
+
+    @property
+    def cor(self):
+        return self._cor
+
+    @property
+    def cor_tampa(self):
+        return self._cor_tampa
+
+    @cor.setter
+    def cor(self, valor):
+        self._cor = valor
+
+    @cor_tampa.setter
+    def cor_tampa(self, valor):
+        self._cor_tampa = valor
+
+
+caneta = Caneta("azul", "preto")
+print(caneta.cor_tampa)  # usando o getter
+print(caneta.cor)
+caneta.cor = "vermelho"  # usando o setter
+print(caneta.cor)
