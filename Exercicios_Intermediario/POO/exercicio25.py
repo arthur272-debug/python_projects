5e0ce43bc57693bd6c383d27f47264c5ac0c73ba
Arthur André <arthurdasilvaandre@gmail.com>
2026-01-12 06:43:54 -0300

    formatando_arquivo

---
+++ Exercicios_Intermediario/POO/exercicio25.py
@@ -0,0 +1,9 @@
+# Exercício com classes
+# 1- Crie uma classe Carro (Nome)
+# 2- Crie uma classe Motor (Nome)
+# 3- Crie uma classe Fabricante (Nome)
+# 4- Faça a ligação entre Carro e Motor
+# OBS.: Um motor pode ser de vários carros
+# 5- Faça a ligação entre Carro e Fabricante
+# OBS.: Um fabricante pode fabricar vários carros
+# Exiba o nome do carro, motor e fabricante na tela
