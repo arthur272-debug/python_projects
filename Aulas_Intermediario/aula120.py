506f037fd133f5aebe74d66759e8bc48b126654e
Arthur André <arthurdasilvaandre@gmail.com>
2026-02-04 14:52:23 -0300

    exercitando_conceito

--- Aulas_Intermediario/aula120.py
+++ Aulas_Intermediario/aula120.py
@@ -1,1 +1,30 @@
 # Aprendendo Herança Simples em Python - Sintaxe
+class Pessoa:
+    cpf = "123456789"
+
+    def __init__(self, nome, idade, sexo):
+        self.nome = nome
+        self.idade = idade
+        self.sexo = sexo
+
+    def expressarComprimento(self):
+        print(f"Olá, meu nome é {self.nome} e tenho {self.idade} anos.")
+
+
+class Maquinista(Pessoa):
+    def expressarComprimento(self):
+        print(
+            f"Olá, meu nome é {self.nome}, tenho {self.idade} anos e sou maquinista de trem."
+        )
+
+
+class Motorista(Pessoa): ...
+
+
+cpf = "987654321"
+
+
+p1 = Maquinista("Thiago", 30, "Masculino")
+p1.expressarComprimento()
+p2 = Motorista("Maria", 25, "Feminino")
+p2.expressarComprimento()
