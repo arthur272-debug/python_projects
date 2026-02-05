aaa35fcb57df54324a556566e48add3b6ef2190f
Arthur André <arthurdasilvaandre@gmail.com>
2026-02-05 11:16:29 -0300

    study_super_sobreposicao_metodos

--- Aulas_Intermediario/aula121.py
+++ Aulas_Intermediario/aula121.py
@@ -1,1 +1,20 @@
 # Praticando a sintaxe 'super' e a sobreposição de métodos
+
+
+class MinhaString(str):
+    def upper(self):
+        resultado = super().upper()
+        print("Usando o método upper da classe pai")
+        return f"String em maiúsculas: {resultado}"
+
+    def lower(self):
+        resultado = super().lower()
+        print("Usando o método lower da classe pai")
+        return f"String em minúsculas: {resultado}"
+
+
+word = MinhaString("Python é Incrível!")
+print(word.upper())
+print(
+    MinhaString.mro()
+)  # Mostra a ordem de resolução de métodos (Method Resolution Order)
